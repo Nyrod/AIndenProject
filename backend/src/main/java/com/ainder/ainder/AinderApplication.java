5f07414b5e668ab9bb63862e153d45d21b77ef41
Michał <fierkamichal@gmail.com>
2017-11-23 23:35:19 +0100

    setup

---
+++ backend/src/main/java/com/ainder/ainder/AinderApplication.java
@@ -0,0 +1,12 @@
+package com.ainder.ainder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AinderApplication.class, args);
+	}
+}
